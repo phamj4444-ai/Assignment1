c72c59cbb2f17573a175a7a5d6f3d6d63f9fa57a
phamj4444-ai <phamj4444@gmail.com>
2026-09-22 21:49:08 +0700

    Update and rename ex1.py to ex2.py

---
+++ ex2.py
@@ -0,0 +1,6 @@
+lenght = int(input("Enter lenght:"))
+width = int(input("Enter width"))
+perimeter = lenght*4
+area = lenght*width
+print("The perimeter is:", perimeter)
+print("The area is:", area)
