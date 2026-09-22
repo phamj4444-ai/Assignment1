db0aabe43d343ff0ed01ce06a22a50451311e593
phamj4444-ai <phamj4444@gmail.com>
2026-09-22 21:46:20 +0700

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,7 @@
+i = int(input("Enter interger_1:"))
+i2 = int(input("Enter interger_2:"))
+i3 = int(input("Enter interger_3:"))
+sum = i+i2+i3
+product = i*i2*i3
+average = sum/3
+print(f'Sum is: {sum}, Product is: {product}, Average is: {average}')
